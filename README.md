c290e2ecdc1d531f2a12c79af118a23520b0121e
Ryan Kim <ryan@Ryans-MacBook-Pro-308.local>
2026-02-05 19:42:30 -0500

    Finished url_frontier.cpp function implementaitons, need validation/normalization

--- README.md
+++ README.md
@@ -4,14 +4,27 @@ TLDR: Web Crawlers naturally form a pipeline, which can be roughly divided into 
 
 Current Progress:
 - Implemented Bounded Blocking Queue
-    - Utilizing <std::mutex> to ensure thread safety
+    - Utilizing '''<std::mutex>''' to ensure thread safety
     - Limiting workers prevents overloading and relieves back pressure, assuming fetching is faster than parsing/storing
     - **Needs Testing**
 - URL Frontier
     - When the workers are visiting different URLs, it is crucial we implement URL deduplication
     - We normalize then insert the shortened URL into our visited set
-    - Uses <std::shared_mutex> since we are doing mostly read-only operations and simultaneous writes of the same content are rare and already accounted for with set uniqueness
+    - Uses '''<std::shared_mutex>''' since we are doing mostly read-only operations and simultaneous writes of the same content are rare and already accounted for with set uniqueness
     - **Needs function implementation in a .cpp file**
+
+Current Thoughts:
+- Realizing I'm going to have to deal with validating/normalizing URLs somewhere, this part isn't super non-trivial it's just I have to figure out where to put it
+- How the hell am I gonna deal with page parsing?
+- Gonna have to deal with the configuration (timeout, size of Bounded Queues), but that will have to be done with unit testing later
+
+Resources I find/found useful:
+- Web Crawlers: https://bytebytego.com/courses/system-design-interview/design-a-web-crawler 
+- Bounded Queue: https://leetcode.com/problems/design-bounded-blocking-queue/description/ (I'm serious)
+- Concurrency: https://www.youtube.com/playlist?list=PLvv0ScY6vfd_ocTP2ZLicgqKnvq50OCXM (Mike Shah is the GOAT)
+- CPP in General: https://www.learncpp.com/ 
+- URL Frontier: https://nlp.stanford.edu/IR-book/html/htmledition/the-url-frontier-1.html
+- Rate Limiter: https://bytebytego.com/courses/system-design-interview/design-a-rate-limiter 
 
 To-Do:
 **Crucial**
--- src/crawler/url_frontier.cpp
+++ src/crawler/url_frontier.cpp
@@ -27,9 +27,77 @@ bool URLFrontier::try_add(const std::string& url, std::chrono::milliseconds time
         urls_added_++;
         return true;
     }
-    
     // If unsuccessful, return false
     return false;
+}
 
-    
+bool URLFrontier::try_add_nowait(const std::string& url){
+    // Need to add check for validity and normality first, will do later in utils
+
+    // Check visited set with read lock first
+    {
+        std::shared_lock<std::shared_mutex> read_lock(visited_mutex_);
+        if (visited_.count(url) > 0) {
+            duplicates_skipped_++;
+            return false;
+        }
+    } // shared_lock goes out of scope here, destructor called, unlocked
+
+    // Not visited
+    {
+        std::unique_lock<std::shared_mutex> write_lock(visited_mutex_);
+        auto [it, inserted] = visited_.insert(url);
+        if (!inserted){
+            duplicates_skipped_++;
+            return false;
+        }
+    }
+
+    // Successfully marked as visited, add to Bounded Queue with no 
+    if (queue_.try_push(url)){
+        urls_added_++;
+        return true;
+    }
+    // If unsuccessful, return false
+    return false;
+}
+
+size_t URLFrontier::add_batch(const std::vector<std::string>& urls){
+    size_t added = 0;
+    for(const auto& url : urls) {
+        if(try_add_nowait(url)) added++;
+    }
+    return added;
+}
+
+std::optional<std::string> URLFrontier::pop(std::chrono::milliseconds timeout){
+    return queue_.pop(timeout);
+}
+
+bool URLFrontier::is_visited(const std::string& url) const {
+    std::shared_lock<std::shared_mutex> lock(visited_mutex_);
+    return visited_.count(url) > 0;
+}
+
+void URLFrontier::mark_visited(const std::string& url) {
+    std::unique_lock<std::shared_mutex> lock(visited_mutex_);
+    visited_.insert(url);
+}
+
+void URLFrontier::shutdown() {
+    queue_.shutdown();
+}
+
+size_t URLFrontier::visited_count() const {
+    std::shared_lock<std::shared_mutex> lock(visited_mutex_);
+    return visited_.size();
+}
+
+bool URLFrontier::is_visited_internal(const std::string& normalized_url) const {
+    return visited_.count(normalized_url) > 0;
+}
+
+bool URLFrontier::add_to_visited(const std::string& normalized_url) {
+    auto [it, inserted] = visited_.insert(normalized_url);
+    return inserted;
 }
